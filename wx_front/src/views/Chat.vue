bb67813811c48e7c0ff7db60dba5795f42ea6370
company <1693889638@qq.com>
2025-02-21 17:36:44 +0800

    群聊弹窗开发

--- wx_front/src/views/Chat.vue
+++ wx_front/src/views/Chat.vue
@@ -7,19 +7,98 @@
     </div>
     <!-- tab 标签页切换 -->
     <nut-tabs v-model="tabIndexValue" swipeable background="#ffffff" size="large">
-      <nut-tab-pane title="全部群聊" pane-key="1"> Content 1 </nut-tab-pane>
+      <nut-tab-pane title="全部群聊" pane-key="1">
+        <!-- 列表展示 -->
+        <div class="qunliao_list" @click="openCahtAllFn">
+          <div class="qunliao_list_info">
+            <img src="https://xp-cdn-oss.oss-cn-wuhan-lr.aliyuncs.com/cookies/头像.png?1737703342307" alt="" />
+            <div class="qunliao_list_info_txt">
+              <span>小潘</span>
+              <span>今天 • 10:30</span>
+            </div>
+          </div>
+          <div class="qunliao_list_message">2</div>
+        </div>
+      </nut-tab-pane>
       <nut-tab-pane title="个人单聊" pane-key="2"> Content 2 </nut-tab-pane>
     </nut-tabs>
+    <!-- 群聊弹出层 -->
+    <nut-popup v-model:visible="chatAllPopupState" position="right" :style="{ width: '100%', height: '100%' }">
+      <div class="chat_all_top">
+        <div class="chat_all_top_txt">
+          <RectLeft @click="closeChatAllPopup" width="4.8vw" height="4.8vw" />
+          <img src="https://xp-cdn-oss.oss-cn-wuhan-lr.aliyuncs.com/cookies/头像.png?1737703342307" alt="" />
+          <div class="chat_all_top_state">
+            <span>小潘</span>
+            <span><i class="chat_all_top_dot"></i>在线</span>
+          </div>
+        </div>
+        <MoreX width="4.8vw" height="4.8vw" />
+      </div>
+    </nut-popup>
   </div>
 </template>
 
 <script setup>
+import { MoreX, RectLeft } from "@nutui/icons-vue";
 import { ref } from "vue";
-const tabIndexValue = ref("1");
+const tabIndexValue = ref("1"); // tab 标签页切换索引
+const chatAllPopupState = ref(true); // 群聊弹出层
+
+// 函数 打开群聊弹出层
+function openCahtAllFn() {
+  chatAllPopupState.value = true;
+}
+// 函数 关闭群聊弹出层
+function closeChatAllPopup() {
+  chatAllPopupState.value = false;
+}
 </script>
 
 <style lang="scss" scoped>
 .Chat {
+  .chat_all_top {
+    padding: 2.6667vw 6.4vw;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 14.9333vw;
+    .chat_all_top_txt {
+      display: flex;
+      align-items: center;
+    }
+    img {
+      width: 8.5333vw;
+      height: 8.5333vw;
+      border-radius: 50%;
+      margin-left: 4.2667vw;
+      margin-right: 2.1333vw;
+    }
+    .chat_all_top_state {
+      display: flex;
+      flex-direction: column;
+      span {
+        &:nth-child(1) {
+          font-size: 4vw;
+        }
+        &:nth-child(2) {
+          font-size: 3.2vw;
+          color: rgba(70, 209, 145, 1);
+          vertical-align: bottom;
+        }
+        .chat_all_top_dot {
+          display: inline-block;
+          width: 2.1333vw;
+          height: 2.1333vw;
+          background-color: rgba(70, 209, 145, 1);
+          border-radius: 50%;
+          margin-right: 1.0667vw;
+        }
+      }
+    }
+  }
   font-family: "pingfang";
   .search_all {
     position: relative;
@@ -50,6 +129,58 @@ const tabIndexValue = ref("1");
       height: 4.4453vw;
       margin-right: 2.6667vw;
       margin-left: 5.3333vw;
+    }
+  }
+  .qunliao_list {
+    width: 87.2vw;
+    height: 21.3333vw;
+    opacity: 1;
+    border-radius: 2.1333vw;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 0px 2.1333vw rgba(0, 0, 0, 0.08);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .qunliao_list_info {
+      display: flex;
+      align-items: center;
+      img {
+        width: 12.8vw;
+        height: 12.8vw;
+        border-radius: 50%;
+        margin-left: 4.2667vw;
+        margin-right: 3.2vw;
+      }
+      .qunliao_list_info_txt {
+        display: flex;
+        flex-direction: column;
+        span {
+          &:nth-child(1) {
+            font-size: 4.2667vw;
+            font-weight: 400;
+            color: #000;
+            margin-bottom: 1.0667vw;
+          }
+          &:nth-child(2) {
+            font-size: 3.2vw;
+            font-weight: 400;
+            letter-spacing: 0px;
+            line-height: 4.2667vw;
+            color: rgba(175, 175, 175, 1);
+          }
+        }
+      }
+    }
+    .qunliao_list_message {
+      width: 6.4vw;
+      height: 6.4vw;
+      border-radius: 50%;
+      background-color: #3f51b5;
+      text-align: center;
+      color: #fff;
+      font-size: 3.2vw;
+      line-height: 6.4vw;
+      margin-right: 4.2667vw;
     }
   }
 }
